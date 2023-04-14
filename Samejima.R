5c3356c7b64532b0129a2a0894af9a15a6296772
Christian Badillo <cris.hernandez1014@comunidad.unam.mx>
2023-04-13 18:38:48 -0600

    Parameter_Estimate

--- Samejima.R
+++ Samejima.R
@@ -127,7 +127,10 @@ simulated_data_GRM <- function(thetas, alphas, item_betas){
 
 generated_data <- simulated_data_GRM(thetas, alphas, item_betas)
 
-write.csv(generated_data, file = "datos.csv")
+write.csv(item_betas, file = "betas.csv")
+write.csv(alphas, file = "alphas.csv")
+write.csv(thetas, file = "thetas.csv")
+write.csv(generated_data, file = "sim_data.csv")
 
 
 #########################################################################################################
@@ -137,76 +140,133 @@ n_per <- dim(generated_data)[1]
 n_items <- dim(generated_data)[2]
 n_cate <- 4 # Número de categorías es igual m-1.
 
+generated_data = generated_data + 1
+View(generated_data)
 
-obs_var <- list("n_per", "n_items", "n_cate", "generated_data")
+data_model = list(
+              J = n_cate,
+              n_per = n_per,
+              n_items = n_items,
+              DATA = generated_data
+)
 
-unobs_var <- c("theta_prior_p", "alpha_prior_i",
-               "theta_pos_p", "alpha_pos_i", 
-               "beta_prior_ij", "beta_pos_ij")
+write("
+data{
+  int<lower=1, upper=5> J; //número de categorías m-1
+  int<lower = 0> n_per; //número de personas
+  int<lower = 0> n_items; //número de ítems
+  int<lower = 1, upper = J> DATA[n_per, n_items];
+}
+parameters{
+    vector[n_per] theta;
+    real<lower=0> alpha [n_items];
+    ordered[J-1] beta[n_items]; //category difficulty
+}
+model{
+    alpha ~ gamma(1.5,2);
+    theta ~ normal(0,1);
+    for (i in 1:n_items){
+      for (j in 1:(J-1)){
+        beta[i,j] ~ normal(0,5);
+      }}
+    for (i in 1:n_per){
+      for (j in 1:n_items){
+        DATA[i,j] ~ ordered_logistic(theta[i]*alpha[j],beta[j]);
+      }}
+}" 
+,"model_samejima.stan")
 
-write(
-    "
-    model{
-    
-    # Priors sobre personas.
-    
-        for (p in 1:n_per){
-            theta_prior_p[p] ~ dnorm(0, 5)
-            theta_pos_p[p] ~ dnorm(0, 5)
-        }
-    
-    # Priors sobre alphas items
-        for (a in 1:n_items){
-            alpha_prior_i[a] ~ dgamma(1.5, 1)  T(0,)
-            alpha_pos_i[a] ~ dgamma(1.5, 1) T(0,)
-        }
-    
-    # Priors sobre betas de categorías e ítems
-        for(i in 1:n_items){
-            for(j in 1:n_cate){
-    
-                beta_prior_ij[i, j] ~ dnorm(0, 5)
-                beta_pos_ij[i, j] ~ dnorm(0, 5)
-            }
-        }
-        
-       # for (i in n_items){
-        #    betas[1:n_cate] <- beta_pos_ij[i, ]
-         #   betas[1:n_cate] <- sort(betas)
-    #
-     #       for (j in 1:n_cate){
-      #          betas_pos[i, j] <- betas[j]
-       # } }
-    
-    # Probabilidades acumuladas
-    
-    for (persona in 1:n_per) {
-            for (item in 1:n_items) {
-                
-                generated_data[persona,item] ~ dordered.logit(alpha_pos_i[item] * theta_pos_p[persona], beta_pos_ij[item,])
-            
-            }
-        }
+library(rstan)
+
+fit <- stan(file = "model_samejima.stan", data = data_model, model_name = "Samejima_Model_Fit", 
+     iter = 8000, warmup = 1000, chains = 4, thin = 2, verbose = T, cores = 6)
+
+suma <- summary(fit) # Descripción de los datos 
+desc <- suma$summary # Visualizar estadísticos importantes.
+
+summary(desc[1:1060, 10]) # Rhat
+summary(desc[1:1060, 9]) # n_effect
+
+theta_means = desc[1:1000, 1] # Expected value Betas.
+alpha_means = desc[1001:1015, 1] # Expected value Alphas.
+beta_means = matrix(data = desc[1016:1060], nrow = 15, ncol = 3, byrow = T) # Expected value Betas.
 
 
-    
-    }    
-", "model_samejima.bug")
+#############################################################################################################################
+# Visualización
+############################
 
-library(R2jags)
 
-initsvalues = list(list(theta_prior_p = runif(1, -1,1), alpha_prior_0= runif(15, 0.5,2), theta_pos_p=runif(1, -1,1), beta_prior_ij=runif(1, -2,2), beta_pos_ij=matrix(0.01, ncol = 4, nrow = 15)), 
-                   list(theta_prior_p = runif(1, -1,1), alpha_prior_i=runif(15, 0.5,2), theta_pos_p=runif(1, -1,1), beta_prior_ij=runif(1, -2,2), beta_pos_ij=matrix(0.1, ncol = 4, nrow = 15)), 
-                   list(theta_prior_p = runif(1, -1,1), alpha_prior_i=runif(15, 0.5,2), theta_pos_p=runif(1, -1,1), beta_prior_ij=runif(1, -2,2), beta_pos_ij=matrix(0.1, ncol = 4, nrow = 15)), 
-                   list(theta_prior_p = runif(1, -1,1), alpha_prior_i=runif(15, 0.5,2), theta_pos_p=runif(1, -1,1), beta_prior_ij=runif(1, -2,2), beta_pos_ij=matrix(0.01, ncol = 4, nrow = 15)))
+person_plot = c()
 
-set.seed(10156542)
-bayes_samejima <- jags(
-    data = obs_var, inits = initsvalues,
-    parameters.to.save = unobs_var,
-    model.file = "model_samejima.bug",
-    n.chains = 4,
-    n.iter = 25000,
-    n.burnin = 2000,
-    n.thin = 4 ) 
+set.seed(25)
+person = sample(x = 1:1000, size = 20, replace = T)
+person = sort(person)
+
+for (i in 1:20) {
+  stri = paste("theta[", round(person[i]), "]", sep = "")
+  person_plot[i] = stri
+}
+
+plot(fit, show_density = T, ci_level = 0.95, fill_color = "#AFDEA2", pars = "alpha", point_est = "mean") # Densities alphas
+plot(fit, show_density = T, ci_level = 0.95, fill_color = "#AFDEA2", pars = "beta", point_est = "mean") # Densities betas
+plot(fit, show_density = T, ci_level = 0.95, fill_color = "#AFDEA2", pars = person_plot, point_est = "mean") # Densities thetas
+
+########################################################################################################################
+# Comparación de Resultados
+###################################
+# Theta de las personas
+###########
+
+col_theta = "#D78D50"
+plot(NULL, ann = F, axes = F, xlim = c(-3.5, 4), ylim = c(-4, 4))
+axis(1, at = c(-3.5, -3, -2.5, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4))
+axis(2, las = 1)
+abline(a = 0, b = 1, lty = 'dashed', col = "red", lw = 2)
+points(thetas, theta_means, pch = 21, 
+       bg = paste(col_theta,'aa',sep=''), cex=1.5, scatter = T)
+mtext('\u03B8 Real', 1,cex=1.5,line=3)
+mtext('\u03B8 Estimada',2,cex=1.5,line=2.5)
+mtext('\u03B8 Real vs \u03B8 Estimada',3,cex=2, line = 1.15)
+text(x = 1.5, y = -0.5, paste("\u03C1: ", round(cor(thetas, theta_means), 3), sep = ""), cex = 2)
+
+
+###################################
+# Alphas de los ítems
+###########
+
+
+col_alpha = "#BD2629"
+plot(NULL, ann = F, axes = F, xlim = c(0, 2.5), ylim = c(0, 2.5))
+axis(1, at = c(0, 0.5, 1, 1.5, 2, 2.5))
+axis(2, las = 1)
+abline(a = 0, b = 1, lty = 'dashed', col = "#85CF00", lw = 2)
+points(alphas, alpha_means, pch = 21, 
+       bg = paste(col_alpha,'aa',sep=''), cex=1.5, scatter = T)
+mtext('\u03B1 Real', 1,cex=1.5,line=3)
+mtext('\u03B1 Estimada',2,cex=1.5,line=2.5)
+mtext('\u03B1 Real vs \u03B1 Estimada',3,cex=2, line = 1.15)
+text(x = 1.5, y = 1, paste("\u03C1: ", round(cor(alphas, alpha_means), 3), sep = ""), cex = 2)
+
+
+###################################
+# Betas de las Categorías
+###########
+
+
+col_beta = "#0078BD"
+
+par(mfrow = c(3, 5))
+
+for (i in 1:15) {
+  plot(NULL, ann = F, axes = F, xlim = c(-4, 5), ylim = c(-5, 7), new = T)
+  axis(1)
+  axis(2, las = 1)
+  abline(a = 0, b = 1, lty = 'dashed', col = "#E78140", lw = 2)
+  points(item_betas[i, ], beta_means[i, ], pch = 21, 
+         bg = paste(col_beta,'aa',sep=''), cex=3, scatter = T)
+  mtext('\u03B2 Real', 1,cex=1.5,line=3)
+  mtext('\u03B2 Estimada',2,cex=1.5,line=2.5)
+  mtext(paste('Betas Item: ', i, sep = ""),3,cex=2, line = 1.15)
+}
 
